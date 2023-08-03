6b6b67d3f074f0781ecf097a6ac90d26e7ffb3fa
Ayanda-boluwatife <ayandastephen1000@gmail.com>
2023-08-03 09:54:14 +0100

    updates on the code

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,13 @@
-"use client"
+import React from 'react';
+import ApiList from '@/components/ApiList';
 
-import {React, useState } from 'react'
-import ApiList from '@/components/ApiList'
-import ApiDetails from '@/components/ApiDetails'
-
-export default function Home() {
-  const [selectedAPI, setSelectedAPI] = useState(null);
-
-  const showAPIDetails = (api) => {
-    setSelectedAPI(api);
-  };
-
+const IndexPage = () => {
   return (
-    <div className=" w-full bg-white p-4">
+    <div className="container mx-auto w-full p-4">
       <h1 className="text-3xl font-semibold text-center mb-8">API Interface</h1>
-      <div className="">
-        <ApiList showAPIDetails={showAPIDetails} />
-        {selectedAPI && <ApiDetails api={selectedAPI} />}
-      </div>
+      <ApiList />
     </div>
   );
-}
+};
+
+export default IndexPage;
--- src/components/ApiList.jsx
+++ src/components/ApiList.jsx
@@ -1,12 +1,16 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import ApiDetails from './ApiDetails';
 
-const apiUrl = 'https://api.publicapis.org/entries';
+const apiUrl = 'https://api.publicapis.org/entries'; // Replace this with the actual API endpoint
 
-const ApiList = ({ showAPIDetails }) => {
+const ApiList = () => {
   const [apiData, setApiData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [selectedAPI, setSelectedAPI] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchAPIs = async () => {
@@ -22,22 +26,25 @@ const ApiList = ({ showAPIDetails }) => {
     fetchAPIs();
   }, []);
 
+  // Constants for pagination
   const itemsPerPage = 50;
-  const totalPages = Math.ceil(apiData.length / itemsPerPage);
-  const [currentPage, setCurrentPage] = useState(1);
 
+  // Function to handle pagination click
   const handlePageClick = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  // Function to handle search input
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
-    setCurrentPage(1); 
+    setCurrentPage(1); // Reset to the first page when searching
   };
 
+  // Function to get the range of page numbers to display
   const getPageRange = () => {
     const totalButtons = 5;
     const middleButton = Math.floor(totalButtons / 2);
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
     let startPage = currentPage - middleButton;
     let endPage = currentPage + middleButton;
@@ -55,12 +62,24 @@ const ApiList = ({ showAPIDetails }) => {
     return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
   };
 
+  // Filter the API data based on the search query
   const filteredData = apiData.filter(
     (api) =>
       api.API.toLowerCase().includes(searchQuery.toLowerCase()) ||
       api.Description.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  // Function to handle API list item click and open the modal
+  const handleAPIItemClick = (api) => {
+    setSelectedAPI(api);
+  };
+
+  // Function to close the modal
+  const handleCloseModal = () => {
+    setSelectedAPI(null);
+  };
+
+  // Function to handle pagination and display 50 items at a time
   const getPaginatedData = () => {
     const start = (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
@@ -73,7 +92,7 @@ const ApiList = ({ showAPIDetails }) => {
         <input
           type="text"
           placeholder="Search APIs..."
-          className="px-2 py-1 border bg-transparent rounded"
+          className="px-2 py-1 border rounded"
           value={searchQuery}
           onChange={handleSearch}
         />
@@ -83,7 +102,7 @@ const ApiList = ({ showAPIDetails }) => {
           <div
             key={index}
             className="p-4 w-full md:w-auto border rounded cursor-pointer"
-            onClick={() => showAPIDetails(api)}
+            onClick={() => handleAPIItemClick(api)}
           >
             <h2 className="text-xl font-semibold">{api.API}</h2>
             <p>{api.Description}</p>
@@ -99,18 +118,19 @@ const ApiList = ({ showAPIDetails }) => {
             Prev
           </button>
         )}
-        {getPageRange().map((pageNumber) => (
-          <button
-            key={pageNumber}
-            className={`mx-1 px-2 py-1 rounded ${
-              currentPage === pageNumber ? 'bg-blue-500 text-white' : 'bg-gray-300'
-            }`}
-            onClick={() => handlePageClick(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        ))}
-        {currentPage < totalPages && (
+        {filteredData.length > 0 &&
+          getPageRange().map((pageNumber) => (
+            <button
+              key={pageNumber}
+              className={`mx-1 px-2 py-1 rounded ${
+                currentPage === pageNumber ? 'bg-blue-500 text-white' : 'bg-gray-300'
+              }`}
+              onClick={() => handlePageClick(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          ))}
+        {currentPage < Math.ceil(filteredData.length / itemsPerPage) && (
           <button
             className="mx-1 px-2 py-1 rounded bg-gray-300"
             onClick={() => handlePageClick(currentPage + 1)}
@@ -119,6 +139,18 @@ const ApiList = ({ showAPIDetails }) => {
           </button>
         )}
       </div>
+      <Modal
+        isOpen={selectedAPI !== null}
+        onRequestClose={handleCloseModal}
+        contentLabel="API Details Modal"
+        className="modal-content absolute"
+        overlayClassName="modal-overlay"
+      >
+        {selectedAPI && <ApiDetails api={selectedAPI} />}
+        <button className="modal-close-btn" onClick={handleCloseModal}>
+          Close
+        </button>
+      </Modal>
     </section>
   );
 };
